b86a64a38a53c138b999e03b39fdc27079e0977e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-26 23:35:04 +0000

    refactor: Add a placeholder for the exeption variable

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
 
-
 /**
  * Simulate a signup process
  */
@@ -8,7 +7,7 @@ export default async function handleProfileSignup() {
   try {
     const values = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  } catch {
+  } catch (error) {
     console.log('Signup system offline');
   }
 }
