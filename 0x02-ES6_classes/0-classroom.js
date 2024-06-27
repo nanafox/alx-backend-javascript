666748b76997ee579e8693e66c3ff5be340ecb84
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-27 19:31:29 +0000

    refactor: Rename maxStudentSize to maxStudentsSize in ClassRoom constructor

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -4,16 +4,16 @@
 export default class ClassRoom {
   /**
    * Represents a class constructor for setting the maximum student size.
-   * @param {number} maxStudentSize - The maximum number of students allowed in the class.
+   * @param {number} maxStudentsSize - The maximum number of students allowed in the class.
    */
-  constructor(maxStudentSize) {
-    if (typeof maxStudentSize !== 'number') {
+  constructor(maxStudentsSize) {
+    if (typeof maxStudentsSize !== 'number') {
       throw new Error('maxStudentSize must be a number');
     }
-    if (maxStudentSize <= 0) {
+    if (maxStudentsSize <= 0) {
       throw new Error('maxStudentSize must be greater than 0');
     }
 
-    this._maxStudentSize = maxStudentSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
