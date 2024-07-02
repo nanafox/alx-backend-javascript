d7c8af5f1bc8c92449af3a16ab5b4771cf1dc19f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-02 14:21:26 +0000

    feat: Add a function that return 3 student objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieves a list of students.
+ *
+ * This function returns an array of student objects with their id, first
+ * name, and location.
+ *
+ * @returns {Array} An array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbina' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
