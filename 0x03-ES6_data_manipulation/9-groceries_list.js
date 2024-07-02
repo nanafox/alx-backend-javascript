d04eff1f33d7e73321090f917b55b217fea0ee27
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-02 18:42:24 +0000

    feat: Add groceriesList function to create a Map of grocery items and quantities

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+/**
+ * Creates a groceries list with items and quantities using a Map.
+ *
+ * @returns {Map} - A Map containing grocery items as keys and their quantities as values.
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Bananas', 5],
+  ]);
+}
