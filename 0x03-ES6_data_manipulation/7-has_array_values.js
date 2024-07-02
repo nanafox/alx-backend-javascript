1b6b94cbd0a46511a04269cd58b7fed3969888e0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-02 18:17:56 +0000

    feat: Add hasValuesFromArray function to check if a Set has all the values from an array

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+/**
+ * Checks if a Set has all the values from an array.
+ *
+ * @param {Set} set - The Set to check for values.
+ * @param {Array} array - The array of values to check against the Set.
+ * @returns {boolean} - True if the Set contains all values from the array, false otherwise.
+ */
+export default function hasValuesFromArray(set, array) {
+  return set.isSupersetOf(new Set(array));
+}
